b71618cd5004c5e07c7db4de214581883fead637
jekvodogrei <jekvodogrei16@gmail.com>
2023-05-29 14:45:10 +0300

    create button

--- app/src/main/java/com/example/new_sapper_game/MainActivity.kt
+++ app/src/main/java/com/example/new_sapper_game/MainActivity.kt
@@ -1,121 +1,62 @@
 package com.example.new_sapper_game
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.recyclerview.widget.RecyclerView
-import com.example.new_sapper_game.ui.theme.New_sapper_gameTheme
+import java.util.Random
+import android.widget.Toast
+
+
 
 class MainActivity : ComponentActivity() {
+
+    private var buttons: Array<TextView>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activiti_main)
+        setContentView(R.layout.activity_main)
 
-            setContent {
-            New_sapper_gameTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                 SaperApp()
-                }
+        createButtons()
+
+    }
+
+    fun createButtons() {
+        buttons = Array(64) {
+            findViewById<TextView?>(getButtonId(it + 1))
+        }
+
+
+        var minesLeft = 10
+        var emptyButton = 64
+
+
+        while (emptyButton > 0) {
+            val buttonIndex = Random().nextInt(buttons!!.size)
+            val btn = buttons!![buttonIndex]
+
+            if (btn.text.toString() != " ") {
+                btn.setOnClickListener{btn.text = "no" }
+
+                emptyButton--
+            }
+        }
+
+        while (minesLeft > 0) {
+
+            val mineIndex = Random().nextInt(buttons!!.size)
+            var mineButton = buttons!![mineIndex]
+            val duration = 1000
+            var toast = Toast.makeText(applicationContext, "Over", duration)
+
+            if (mineButton.text.toString() != "MINE") {
+                mineButton.setOnClickListener{mineButton.text = "M"}
+                mineButton = toast
+
+                minesLeft--
             }
         }
     }
-}
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun SaperApp() {
-    Surface(modifier = Modifier){
-        CreatingButtons(
-            onButtonClick = {
+    private fun getButtonId(index: Int): Int = this.resources.getIdentifier("button$index", "id", this.packageName)
 
-            }
-        )
-    }
-}
-
-@Composable
-fun CreatingButtons(
-    onButtonClick: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    val buttons = arrayOf(R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9, R.id.button10,
-        R.id.button11, R.id.button12, R.id.button13, R.id.button14, R.id.button15, R.id.button16, R.id.button17, R.id.button18, R.id.button19, R.id.button20,
-        R.id.button21, R.id.button22, R.id.button23, R.id.button24, R.id.button25, R.id.button26, R.id.button27, R.id.button28, R.id.button29, R.id.button30,
-        R.id.button31, R.id.button32, R.id.button33, R.id.button34, R.id.button35, R.id.button36, R.id.button37, R.id.button38, R.id.button39, R.id.button40,
-        R.id.button41, R.id.button42, R.id.button43, R.id.button44, R.id.button45, R.id.button46, R.id.button47, R.id.button48, R.id.button49, R.id.button50,
-        R.id.button51, R.id.button52, R.id.button53, R.id.button54, R.id.button55, R.id.button56, R.id.button57, R.id.button58, R.id.button59, R.id.button60,
-        R.id.button61, R.id.button62, R.id.button63, R.id.button64)
-
-    fun bombs(){
-        for (i in buttons) {
-
-        }
-    }
-
-    Box(
-        modifier = modifier
-    ) {
-
-        Column(
-            modifier = Modifier
-        ) {
-            for (i in buttons) {
-
-                Button(
-                    onClick = onButtonClick,
-
-                ) {
-
-                }
-            }
-        }
-    }
-}
-
-
-
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    New_sapper_gameTheme {
-        SaperApp()
-    }
 }
